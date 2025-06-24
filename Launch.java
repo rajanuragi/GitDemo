e2f95476726d48e3439442fadd29bac57d9bbd67
rajanuragi <anuragi1202@gmail.com>
2025-06-25 01:50:53 +0530

    modified after cloning

--- Launch.java
+++ Launch.java
@@ -3,4 +3,12 @@ pubic static void main(String[] args)
 {
 	system.out.println(" welcome ");
 }
+	pubic static void fun()
+	{
+		int a=10;
+		int b=20;
+		int sum =a+b;
+		system.out.println(sum);
+	}
+
 }
